555b21284fa72da544404a95f942db71ff4835b2
Mason <freemason@gmail.com>
2012-07-22 15:24:00 -0700

    bit of cleanup

--- KittenWrangler/KittenWrangler/KWLevel.m
+++ KittenWrangler/KittenWrangler/KWLevel.m
@@ -82,7 +82,7 @@ static const int kKWTimeLimitLevelCost  = 5;
 - (void) free:(NSArray*)kits {
     [kits enumerateObjectsUsingBlock:^(KWKitten* kitten, NSUInteger idx, BOOL *stop) {
         [baskets enumerateObjectsUsingBlock:^(KWBasket* basket, NSUInteger idx, BOOL *stop) {
-            [basket.kittens makeObjectsPerformSelector:@selector(removeKitten:) withObject:kitten];
+            [basket.kittens makeObjectsPerformSelector:@selector(removeObject:) withObject:kitten];
         }];
         kitten.captured = NO;
         [kittens addObject:kitten];
