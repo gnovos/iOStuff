0d3b1381d5bbde902c096afb68b20ab3b5fa9282
Mason <freemason@gmail.com>
2012-08-13 14:32:50 -0700

    just whatever stuff

--- Catpcha/Catpcha/KWSpriteAnimation.h
+++
@@ -1,8 +0,0 @@
-
-@interface KWSpriteAnimation : NSObject
-
-- (id) initWithTimePerFrame:(CGFloat)timePerFrame framesNamed:(NSArray*)names;
-- (void) update:(NSTimeInterval)dt;
-- (GLKTextureInfo*) frame;
-
-@end
