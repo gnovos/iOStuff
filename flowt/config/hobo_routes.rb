14a54f7c04f31d5159ae36d9ea5b9ec4e001ccbe
Mason <freemason@gmail.com>
2012-09-07 16:05:04 -0700

    adding flowt

---
+++ flowt/config/hobo_routes.rb
@@ -0,0 +1,78 @@
+# This is an auto-generated file: don't edit!
+# You can add your own routes in the config/routes.rb file
+# which will override the routes in this file.
+
+Flowt::Application.routes.draw do
+
+
+  # Resource routes for controller users
+  resources :users, :only => [:edit, :show, :create, :update, :destroy] do
+    collection do
+      post 'signup', :action => 'do_signup'
+      get 'signup'
+    end
+    member do
+      get 'account'
+      put 'activate', :action => 'do_activate'
+      get 'activate'
+      put 'reset_password', :action => 'do_reset_password'
+      get 'reset_password'
+    end
+  end
+
+  # User routes for controller users
+  match 'login(.:format)' => 'users#login', :as => 'user_login'
+  get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
+  match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
+
+  namespace :admin do
+
+
+    # Resource routes for controller admin/users
+    resources :users
+
+  end
+
+
+
+# These are the Hobo 1.3 style routes.   They've been included for backwards
+# compatibility reasons because the names of some named routes have been changed.
+# Please update your views to use the new named route names and then disable this
+# section by setting config.hobo.dont_emit_deprecated_routes = true.
+
+
+ 
+
+  # DEPRECATED Lifecycle routes for controller users
+  post 'users/signup(.:format)' => 'users#do_signup', :as => 'do_user_signup'
+  get 'users/signup(.:format)' => 'users#signup', :as => 'user_signup'
+  put 'users/:id/activate(.:format)' => 'users#do_activate', :as => 'do_user_activate'
+  get 'users/:id/activate(.:format)' => 'users#activate', :as => 'user_activate'
+  put 'users/:id/reset_password(.:format)' => 'users#do_reset_password', :as => 'do_user_reset_password'
+  get 'users/:id/reset_password(.:format)' => 'users#reset_password', :as => 'user_reset_password'
+
+  # DEPRECATED Show action routes for controller users
+  get 'users/:id/account(.:format)' => 'users#account', :as => 'user_account'
+
+  # DEPRECATED Resource routes for controller users
+  get 'users/:id/edit(.:format)' => 'users#edit', :as => 'edit_user'
+  get 'users/:id(.:format)' => 'users#show', :as => 'user', :constraints => { :id => %r([^/.?]+) }
+  post 'users(.:format)' => 'users#create', :as => 'create_user'
+  put 'users/:id(.:format)' => 'users#update', :as => 'update_user', :constraints => { :id => %r([^/.?]+) }
+  delete 'users/:id(.:format)' => 'users#destroy', :as => 'destroy_user', :constraints => { :id => %r([^/.?]+) }
+
+  namespace :admin do
+
+
+    # DEPRECATED Resource routes for controller admin/users
+    get 'users(.:format)' => 'users#index', :as => 'users'
+    get 'users/new(.:format)' => 'users#new', :as => 'new_user'
+    get 'users/:id/edit(.:format)' => 'users#edit', :as => 'edit_user'
+    get 'users/:id(.:format)' => 'users#show', :as => 'user', :constraints => { :id => %r([^/.?]+) }
+    post 'users(.:format)' => 'users#create', :as => 'create_user'
+    put 'users/:id(.:format)' => 'users#update', :as => 'update_user', :constraints => { :id => %r([^/.?]+) }
+    delete 'users/:id(.:format)' => 'users#destroy', :as => 'destroy_user', :constraints => { :id => %r([^/.?]+) }
+
+  end
+
+end
---
+++ flowt/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowt::Application.config.secret_token = '54ae6ec6be7324143c471d3538bc1a6d12c6f9be8d40838de06d0eee7b0ac5450aee86e79578726f75092f41c8b21231a25429e32220a59027e203079822a736'
